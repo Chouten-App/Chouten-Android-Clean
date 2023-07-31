55806564fe5f08e6c528afcca737d84bfd87b255
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-07-31 23:14:56 +0100

    fix: 🐛 UiText parcelable
    UiText not being parcelable caused some issues with the app crashing when sent to the background

--- app/src/main/java/com/chouten/app/common/UiText.kt
+++ app/src/main/java/com/chouten/app/common/UiText.kt
@@ -1,27 +1,29 @@
 package com.chouten.app.common
 
 import android.content.Context
-import androidx.annotation.StringRes
+import android.os.Parcelable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import kotlinx.parcelize.Parcelize
 
-sealed class UiText {
+@Parcelize
+sealed class UiText : Parcelable {
     class Literal(val string: String) : UiText()
-    class StringRes(@androidx.annotation.StringRes val stringRes: Int, vararg val args: Any) :
+    class StringRes(@androidx.annotation.StringRes val stringRes: Int) :
         UiText()
 
     @Composable
     fun string(): String {
         return when (this) {
             is Literal -> this.string
-            is StringRes -> stringResource(this.stringRes, *this.args)
+            is StringRes -> stringResource(this.stringRes)
         }
     }
 
     fun string(context: Context): String {
         return when (this) {
             is Literal -> this.string
-            is StringRes -> context.getString(this.stringRes, *this.args)
+            is StringRes -> context.getString(this.stringRes)
         }
     }
 }
