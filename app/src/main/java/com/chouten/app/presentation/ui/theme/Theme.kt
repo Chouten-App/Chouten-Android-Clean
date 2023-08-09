457a51e8c59fb65b32251c8b48ef6c83abcf2219
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-08-09 19:04:59 +0100

    feat: 🎉 add default colorscheme

--- app/src/main/java/com/chouten/app/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/chouten/app/presentation/ui/theme/Theme.kt
@@ -24,27 +24,72 @@ import com.chouten.app.domain.proto.AppearancePreferences
 import com.chouten.app.presentation.ui.screens.more.screens.appearance_screen.AppearanceViewModel
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+private val DarkColorScheme =
+    darkColorScheme(
+        primary = md_dark_primary,
+        onPrimary = md_dark_onPrimary,
+        primaryContainer = md_dark_primaryContainer,
+        onPrimaryContainer = md_dark_onPrimaryContainer,
+        inversePrimary = md_dark_inversePrimary,
+        secondary = md_dark_secondary,
+        onSecondary = md_dark_onSecondary,
+        secondaryContainer = md_dark_secondaryContainer,
+        onSecondaryContainer = md_dark_onSecondaryContainer,
+        tertiary = md_dark_tertiary,
+        onTertiary = md_dark_onTertiary,
+        tertiaryContainer = md_dark_tertiaryContainer,
+        onTertiaryContainer = md_dark_onTertiaryContainer,
+        background = md_dark_background,
+        onBackground = md_dark_onBackground,
+        surface = md_dark_surface,
+        onSurface = md_dark_onSurface,
+        surfaceVariant = md_dark_surfaceVariant,
+        onSurfaceVariant = md_dark_onSurfaceVariant,
+        surfaceTint = md_dark_surfaceTint,
+        inverseSurface = md_dark_inverseSurface,
+        inverseOnSurface = md_dark_inverseOnSurface,
+        error = md_dark_error,
+        onError = md_dark_onError,
+        errorContainer = md_dark_errorContainer,
+        onErrorContainer = md_dark_onErrorContainer,
+        outline = md_dark_outline,
+        outlineVariant = md_dark_outlineVariant,
+        scrim = md_dark_scrim,
+    )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+private val LightColorScheme =
+    lightColorScheme(
+        primary = md_light_primary,
+        onPrimary = md_light_onPrimary,
+        primaryContainer = md_light_primaryContainer,
+        onPrimaryContainer = md_light_onPrimaryContainer,
+        inversePrimary = md_light_inversePrimary,
+        secondary = md_light_secondary,
+        onSecondary = md_light_onSecondary,
+        secondaryContainer = md_light_secondaryContainer,
+        onSecondaryContainer = md_light_onSecondaryContainer,
+        tertiary = md_light_tertiary,
+        onTertiary = md_light_onTertiary,
+        tertiaryContainer = md_light_tertiaryContainer,
+        onTertiaryContainer = md_light_onTertiaryContainer,
+        background = md_light_background,
+        onBackground = md_light_onBackground,
+        surface = md_light_surface,
+        onSurface = md_light_onSurface,
+        surfaceVariant = md_light_surfaceVariant,
+        onSurfaceVariant = md_light_onSurfaceVariant,
+        surfaceTint = md_light_surfaceTint,
+        inverseSurface = md_light_inverseSurface,
+        inverseOnSurface = md_light_inverseOnSurface,
+        error = md_light_error,
+        onError = md_light_onError,
+        errorContainer = md_light_errorContainer,
+        onErrorContainer = md_light_onErrorContainer,
+        outline = md_light_outline,
+        outlineVariant = md_light_outlineVariant,
+        scrim = md_light_scrim,
+    )
 
-    /* Other default colors to override
-background = Color(0xFFFFFBFE),
-surface = Color(0xFFFFFBFE),
-onPrimary = Color.White,
-onSecondary = Color.White,
-onTertiary = Color.White,
-onBackground = Color(0xFF1C1B1F),
-onSurface = Color(0xFF1C1B1F),
-*/
-)
 
 @Composable
 fun ChoutenTheme(
