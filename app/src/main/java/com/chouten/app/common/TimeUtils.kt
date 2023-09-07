907e76549b05ea63ee3e45dd95639607c787dad3
Adolar0042 <adolar0042@gmail.com>
2023-09-07 18:16:57 +0200

    feat(LogView): add proper LogView UI
    feat(ui): logView cards
    
    fix: 🐛 log expansion state
    
    chore: 🔧 format
    
    fix: 🐛 reset expanded state on deletion
    
    feat(TimeUtils): epochMillisToTime for human readable timestamps
    
    fix: migrate to epochMillisToTime
    
    fix(design): some changes to things here and there
    
    deps: ⬆️ upgrade AGP
    
    fix: 🐛 reduce timestamp Modifier.weight; ellipses overflow for header
    
    feat(LogEntry): 🎉 add `Serializable` annotation
    
    fix(LogEntry): 🐛 add ellipses if header is clipped content
    
    feat(LogViewModel): 🎉 add export to json file
    
    feat(LogView): 🎉 scaffold with actions
    
    feat(LogView): 🎉 add export action
    
    refactor(LogView): ♻️  clean-up
    
    refactor(LogView): ♻️  migrate to localised strings

---
+++ app/src/main/java/com/chouten/app/common/TimeUtils.kt
@@ -0,0 +1,31 @@
+package com.chouten.app.common
+
+import android.os.Build
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+/**
+ * Converts epoch seconds to a formatted time string.
+ *
+ * @param epochMilli The epoch time in seconds.
+ * @param pattern The desired pattern for formatting the time (default is "h:mm:ss").
+ * @return The formatted time string of [epochMilli] in the pattern of [pattern].
+ */
+fun epochMillisToTime(epochMilli: Long, pattern: String = "HH:mm:ss"): String {
+    val userTimeZone = TimeZone.getDefault()
+
+    return if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+        DateTimeFormatter.ofPattern(pattern)
+            .withLocale(Locale.getDefault())
+            .withZone(userTimeZone.toZoneId())
+            .format(Instant.ofEpochMilli(epochMilli))
+    } else {
+        SimpleDateFormat(pattern, Locale.getDefault()).apply {
+            timeZone = userTimeZone
+        }.format(Date(epochMilli))
+    }
+}
--- app/src/main/java/com/chouten/app/domain/model/LogEntry.kt
+++ app/src/main/java/com/chouten/app/domain/model/LogEntry.kt
@@ -2,11 +2,13 @@ package com.chouten.app.domain.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
 
 @Entity
+@Serializable
 data class LogEntry(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val entryContent: String,
-    val entryHeader: String = entryContent.take(15),
+    val entryHeader: String = "${entryContent.take(15)}...",
     val entryTimestamp: Long = System.currentTimeMillis()
 )
