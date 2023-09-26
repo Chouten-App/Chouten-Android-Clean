2dc2d996782e67f5fb0465a18765a4eee9adfee0
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-09-26 17:50:02 +0100

    fix(InfoView): 🐛 clear InfoViewModel on each load

--- app/src/main/java/com/chouten/app/presentation/ui/screens/info/InfoView.kt
+++ app/src/main/java/com/chouten/app/presentation/ui/screens/info/InfoView.kt
@@ -6,13 +6,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import com.chouten.app.common.Navigation
 import com.chouten.app.common.Resource
 import com.chouten.app.domain.model.SnackbarModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import dagger.hilt.android.internal.managers.FragmentComponentManager.findActivity
 
 @Destination(
     route = Navigation.InfoRoute
@@ -25,9 +24,12 @@ fun InfoView(
     snackbarLambda: (SnackbarModel) -> Unit
 ) {
     val context = LocalContext.current
-    val infoViewModel: InfoViewModel = hiltViewModel(
-        findActivity(context) as ViewModelStoreOwner,
-    )
+    // We don't want to bind the ViewModel to the NavGraph because
+    // it will result in us calling methods on a destroyed Webview.
+    // Clear the InfoViewModel from the ViewModelStore
+    // TODO: Investigate if this messes up other viewmodels
+    LocalViewModelStoreOwner.current?.viewModelStore?.clear()
+    val infoViewModel: InfoViewModel = hiltViewModel()
 
     val infoResults by infoViewModel.infoResults.collectAsState()
     val episodeList by infoViewModel.episodeList.collectAsState()
