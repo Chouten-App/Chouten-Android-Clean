2146602b4ace8eeb76fd67c1b5b7e591242a7554
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-10-06 18:43:28 +0100

    feat(WebviewModuleV2): 🎉 add watch handlers

--- app/src/main/java/com/chouten/app/di/WebviewModuleV2.kt
+++ app/src/main/java/com/chouten/app/di/WebviewModuleV2.kt
@@ -5,6 +5,7 @@ import com.chouten.app.domain.model.Payloads_V2
 import com.chouten.app.domain.repository.WebviewHandler
 import com.chouten.app.presentation.ui.screens.info.InfoResult
 import com.chouten.app.presentation.ui.screens.search.SearchResult
+import com.chouten.app.presentation.ui.screens.watch.WatchResult
 import com.lagradost.nicehttp.Requests
 import dagger.Module
 import dagger.Provides
@@ -43,6 +44,32 @@ object WebviewModuleV2 {
     @OptIn(ExperimentalSerializationApi::class)
     @Provides
     fun provideInfoEpListWebviewHandler(client: Requests): WebviewHandler<Payloads_V2.Action_V2, Payloads_V2.GenericPayload<List<InfoResult.MediaListItem>>> {
+        val json = Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+            explicitNulls = false
+        }
+        return WebviewHandlerImpl(client) { action, result: String ->
+            Payloads_V2.GenericPayload(action, json.decodeFromString(result))
+        }
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    fun provideServerWebviewHandler(client: Requests): WebviewHandler<Payloads_V2.Action_V2, Payloads_V2.GenericPayload<List<WatchResult.ServerData>>> {
+        val json = Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+            explicitNulls = false
+        }
+        return WebviewHandlerImpl(client) { action, result: String ->
+            Payloads_V2.GenericPayload(action, json.decodeFromString(result))
+        }
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    fun provideSourceWebviewHandler(client: Requests): WebviewHandler<Payloads_V2.Action_V2, Payloads_V2.GenericPayload<WatchResult>> {
         val json = Json {
             ignoreUnknownKeys = true
             isLenient = true
