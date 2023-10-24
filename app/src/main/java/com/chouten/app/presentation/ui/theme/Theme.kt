b5a041bff1173b46e4951f2cf09e4ff9564314b2
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-10-24 02:40:49 +0100

    perf(Theme): ⚡️ utilise remember

--- app/src/main/java/com/chouten/app/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/chouten/app/presentation/ui/theme/Theme.kt
@@ -3,8 +3,11 @@ package com.chouten.app.presentation.ui.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModelStoreOwner
 import com.chouten.app.common.FallbackThemes
 import com.chouten.app.common.isDarkTheme
 import com.chouten.app.domain.proto.AppearancePreferences
@@ -12,29 +15,40 @@ import com.chouten.app.presentation.ui.screens.more.screens.appearance_screen.Ap
 import com.t8rin.dynamic.theme.ColorTuple
 import com.t8rin.dynamic.theme.DynamicTheme
 import com.t8rin.dynamic.theme.rememberDynamicThemeState
+import dagger.hilt.android.internal.managers.FragmentComponentManager.findActivity
 
 @Composable
 fun ChoutenTheme(
-    viewModel: AppearanceViewModel = hiltViewModel(),
     content: @Composable () -> Unit
 ) {
     val context = LocalContext.current
+    val viewModel: AppearanceViewModel = hiltViewModel(
+        findActivity(context) as ViewModelStoreOwner
+    )
+
     val currentAppearance by viewModel.getAppearancePreferences(context).collectAsState(
         initial = AppearancePreferences.DEFAULT
     )
 
-    val dynamicColor = currentAppearance.isDynamicColor
-    val isAmoled = currentAppearance.isAmoled
+    val dynamicColor = rememberSaveable(currentAppearance) { currentAppearance.isDynamicColor }
+    val isAmoled = rememberSaveable(currentAppearance) { currentAppearance.isAmoled }
+    val isDarkTheme = rememberSaveable(currentAppearance) { context.isDarkTheme(currentAppearance) }
 
-    val themeState = rememberDynamicThemeState()
+    val colorTuple = remember {
+        ColorTuple(
+            FallbackThemes.LIGHT.primary
+        )
+    }
+    val themeState = rememberDynamicThemeState(
+        colorTuple
+    )
+
     DynamicTheme(
         state = themeState,
-        defaultColorTuple = ColorTuple(
-            FallbackThemes.LIGHT.primary
-        ),
+        defaultColorTuple = colorTuple,
         content = content,
         dynamicColor = dynamicColor,
-        isDarkTheme = context.isDarkTheme(currentAppearance),
+        isDarkTheme = isDarkTheme,
         amoledMode = isAmoled
     )
 }
