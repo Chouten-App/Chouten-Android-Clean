19a662cbb1f7c8bea8984943505f7c5ae459ad98
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-11-02 01:41:32 +0000

    feat: 🎉 add NoRippleInteractionSource

---
+++ app/src/main/java/com/chouten/app/presentation/ui/components/common/NoRippleInteractionSource.kt
@@ -0,0 +1,13 @@
+package com.chouten.app.presentation.ui.components.common
+
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+// Remove ripple effect from clickable elements
+class NoRippleInteractionSource : MutableInteractionSource {
+    override val interactions: Flow<Interaction> = emptyFlow()
+    override suspend fun emit(interaction: Interaction) {}
+    override fun tryEmit(interaction: Interaction) = true
+}
