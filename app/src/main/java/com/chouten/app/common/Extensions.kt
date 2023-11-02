d4ed02169d5e8772f91835952a69995b52ced144
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-11-02 01:39:57 +0000

    feat(Extension): 🎉 add scale function

--- app/src/main/java/com/chouten/app/common/Extensions.kt
+++ app/src/main/java/com/chouten/app/common/Extensions.kt
@@ -168,3 +168,27 @@ fun Long.formatMinSec(): String {
 internal fun lerp(start: Float, end: Float, amount: Float): Float {
     return (1 - amount) * start + amount * end
 }
+
+/**
+ * Scale x1 from start1..end1 range to start2..end2 range
+ */
+internal fun scale(start1: Float, end1: Float, pos: Float, start2: Float, end2: Float) =
+    lerp(start2, end2, calculateFraction(start1, end1, pos))
+
+/**
+ * Scale x.start, x.endInclusive from a1..b1 range to a2..b2 range
+ */
+internal fun scale(
+    start1: Float,
+    end1: Float,
+    range: ClosedFloatingPointRange<Float>,
+    start2: Float,
+    end2: Float
+) =
+    scale(start1, end1, range.start, start2, end2)..scale(
+        start1,
+        end1,
+        range.endInclusive,
+        start2,
+        end2
+    )
