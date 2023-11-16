446826c0424c77e02c65dad5c86e2a2eeafa1003
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-11-16 12:25:16 +0000

    chore(version): 🔧 update version & code (4 -> 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.chouten.app"
         minSdk = 23
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
         buildConfigField("String", "WEBHOOK_URL", "\"${properties.getProperty("bug_webhook")}\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
