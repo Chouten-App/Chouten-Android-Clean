2d235a31dfbc87b788262d4cadb81b785f024c89
Toby Bridle <foreignspaghettibolognese@gmail.com>
2024-01-25 13:37:43 +0000

    fix(AutoUpdate): 🐛 do not update if version is equal

--- app/src/main/java/com/chouten/app/MainActivity.kt
+++ app/src/main/java/com/chouten/app/MainActivity.kt
@@ -83,7 +83,7 @@ class MainActivity : ComponentActivity() {
                                     moduleUseCases.addModule(updateUrl.toUri()) { event ->
                                         when (event) {
                                             is ModuleInstallEvent.PARSED -> {
-                                                (event.module.version < module.version).also { res ->
+                                                (event.module.version <= module.version).also { res ->
                                                     if (!res) {
                                                         appState.viewModel.runAsync {
                                                             logUseCases.insertLog(
